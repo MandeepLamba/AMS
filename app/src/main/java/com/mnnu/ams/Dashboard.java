2cf95ec49831c1c14e1d6a4b588b8416eacb53ae
MandeepLamba <mandeeplamba225@gmail.com>
2020-10-11 15:39:35 +0530

    Update in Dashboard.java

--- app/src/main/java/com/mnnu/ams/Dashboard.java
+++ app/src/main/java/com/mnnu/ams/Dashboard.java
@@ -3,6 +3,7 @@ package com.mnnu.ams;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Bundle bundle = getIntent().getExtras();
+        TextView textView = findViewById(R.id.nametext);
+        textView.setText(bundle.getString("name"));
     }
 }
